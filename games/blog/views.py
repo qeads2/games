0b9383d9474211cb6f65b94b470790c92e02fafa
NAMINO <adzcx2@naver.com>
2017-09-17 22:38:16 +0900

    Django 기본 설정 및 카드형식 샘플페이지

--- games/blog/views.py
+++ games/blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'blog/dino.html', {})
+
+def post_list(request):	
+    posts = Post.objects.all().order_by('-published_date')
+    return render(request, 'blog/dino.html', {'posts':posts})
+
