42ab761fd82ea36bd1b4ab55d56ba3bfc98b2814
NAMINO <adzcx2@naver.com>
2017-10-06 23:52:36 +0900

    업데이트
    ajax 구현 중

--- games/static/js/scroll.js
+++ games/static/js/scroll.js
@@ -1,3 +1,20 @@
 $(document).ready(function (){
-	
+	console.log("hello");
+	$(".search-query").keyup(function () {
+		var title = $(this).val();
+		console.log(title)
+		$.ajax({
+			url:'/ajax/search_title',
+			data: {
+				'title':title
+			},
+			dataType:'json',
+			success: function(data) {
+				if(result) {
+					alert(result);
+				}
+			}
+
+		});
+	});
 });
